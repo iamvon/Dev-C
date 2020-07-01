eea87d67e5ea992b4d060abd4462e3e8343d0f3a
Tuan Pham Minh <tuanpmhd@gmail.com>
2020-07-01 20:14:11 +0700

    update lab 1

--- assignment/1/scripts.js
+++ assignment/1/scripts.js
@@ -1,10 +1,26 @@
 let button = document.getElementById('guessButton')
+let result = document.getElementById("result")
+let pastGuessesSpan = document.getElementById('pastGuesses')
 let min = 0, max = 10
+let limitGuess = 10
+let pastGuesses = []
 let rand = getRandomInt(min, max) 
 console.log(rand)
 
 function getUserGuess() {
     let userGuess = parseInt(document.getElementById('number').value);
+    pastGuesses.push(userGuess)
+    pastGuessesSpan.innerHTML = pastGuesses
+    limitGuess = limitGuess-1
+    if(limitGuess < 0) {
+        alert("YOU HAVE RUN OUT OF GUESSES!!!")
+        limitGuess = 10
+        pastGuesses = []
+        pastGuessesSpan.innerHTML = pastGuesses
+        result.innerHTML = ""
+        return 
+    }
+    console.log("limitGuess", limitGuess) 
     let check =  userGuess - rand    
     checkMatch(check)
 }
@@ -16,13 +32,15 @@ function getRandomInt(min, max) {
 }
 
 function checkMatch(check) {
-    let result = document.getElementById("result")
     if(check == 0) {
-        result.innerHTML = "Congratulations!! You guessed correctly"
+        result.innerHTML = "Congratulations!! You guessed correctly."
+        result.style.backgroundColor = "lightgreen"
     } else if (check > 0) {
-        result.innerHTML = "Sorry your guess is too high"
+        result.innerHTML = "Sorry your guess is too high. You have " + limitGuess + " guesses left." 
+        result.style.backgroundColor = "red"
     } else if (check < 0) {
-        result.innerHTML = "Sorry your guess is too low"
+        result.innerHTML = "Sorry your guess is too low. You have " + limitGuess + " guesses left."
+        result.style.backgroundColor = "red"
     }   
 }
 
