d281b283b710250298cc5208392b27c53b888e07
Tuan Pham Minh <tuanpmhd@gmail.com>
2020-07-01 20:17:30 +0700

    update assignment 1

--- assignment/1/scripts.js
+++ assignment/1/scripts.js
@@ -26,9 +26,9 @@ function getUserGuess() {
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function checkMatch(check) {
